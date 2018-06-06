f278a15c79657353c3c3f4101db70d73349f38de
Sami EZZEROUALI <sami.ezzerouali@hotmail.com>
2018-06-06 17:02:13 +0200

    simple plot

--- SimulationForKalmanFilter.py
+++ SimulationForKalmanFilter.py
@@ -42,37 +42,25 @@ for k in range(0,250):
 	Vl.append(1)
 	Vr.append(1)
 
+"""
 plt.plot(linspace(0,10,600),Vl)
 plt.plot(linspace(0,10,600),Vr)
 plt.show()
-"""
 plt.plot(linspace(0,10,60),Vr)
 plt.show()
 """
 
 #Vecteurs d etat X=(x,y,teta)
 
-x=[];
-y=[];
-z=[];
-teta=[]
+x=[0];
+y=[0];
+z=[0];
+teta=[0]
 
-x.append(0)
-y.append(0)
-teta.append(0)
 
 for k in range(1,iterations):
 	x.append(x[k-1]+h/2*(Vr[k-1]+Vl[k-1])*cos(teta[k-1]))
 	y.append(y[k-1]+h/2*(Vr[k-1]+Vl[k-1])*sin(teta[k-1]))
 	teta.append(teta[k-1]+h/L*(Vr[k-1]-Vl[k-1]))
-plt.plot()
-i=0;
-while 1==1:
-    if i == 0:
-        line, = plt.plot(x, y,teta)
-    else:
-        line.set_ydata(y)
-    plt.pause(0.01) # pause avec duree en secondes
-    i=1
-
+plt.plot(x,y)
 plt.show()
