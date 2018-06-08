d28ce745031c0f2d1aa405f292cb9b27b8345161
Sami EZZEROUALI <sami.ezzerouali@hotmail.com>
2018-06-08 17:13:13 +0200

    vendredi soir

--- trilaterationSimulation.py
+++ trilaterationSimulation.py
@@ -1,16 +1,15 @@
 """
-__________________________________________________________________________________________________________
-|#########################################################################################################|
-|##																										##|
-|##																										##|
-|##																										##|
-|##			Simulation du calcul de la position par Trilateration										##|
-|##																										##|
-|##																										##|
-|##			Tuteur	: JENN Eric																			##|
-|##			Auteur	: EZZEROUALI Sami																	##|
-|##																										##|
-|#########################################################################################################|
+_______________________________________________________________________________
+|##																			##|
+|##																			##|
+|##																			##|
+|##			Simulation du calcul de la position par Trilateration			##|
+|##																			##|
+|##																			##|
+|##			Tuteur	: JENN Eric												##|
+|##			Auteur	: EZZEROUALI Sami										##|
+|##																			##|
+|#############################################################################|
 """
 
 
@@ -19,8 +18,12 @@ from numpy import *
 from random import * # randint(0,10) return a random value 0<X<10
 #from math import *
 
+#----- System constant --------------
+delta=1000
+Ray=[1700,1150,300,1300]
+noise=0.1
 
-
+#------------------------------------
 
 def anchorsPosition(delta):
 	"""
@@ -31,16 +34,10 @@ def anchorsPosition(delta):
 	_Xaux=[0,0,delta,delta]
 	_Yaux=[0,delta,delta,0]
 	_position=[_Xaux,_Yaux]
+
 	return _position;
-
-
-delta=1000
 vector=anchorsPosition(delta)
 
-xAnchors=vector[0]
-yAnchors=vector[1]
-
-plt.plot(xAnchors,yAnchors,"o",markersize=15,color="red")
 #plt.show()
 
 def drawCercles(_X,_Y,_R):
@@ -55,18 +52,16 @@ def drawCercles(_X,_Y,_R):
 			cx.append(_X[k]+_R[k]*sin(ome[i]))
 			cy.append(_Y[k]+_R[k]*cos(ome[i]))
 	plt.plot(cx,cy,"--",color="skyBlue")
-	# plt.show()
+	plt.show()
 
-Ray=[1700,1150,200*sqrt(2),1300]
-drawCercles(xAnchors,yAnchors,Ray)
 
 def robotMove():
 	t=linspace(0,10,5)
 	x=100*(t)+200
 	y=(t)**3+200
 	plt.plot(x,y,"D",markersize=10,color="green")
-	plt.show()
-robotMove()
+	print "Last robot cordinates :"
+	print [x[4],y[4]]
 
 def distanceSensors(rate,ray):
 	dvector=[]
@@ -76,13 +71,64 @@ def distanceSensors(rate,ray):
 
 #print distanceSensors(0.1,[1000,1000,1000,1000])
 
-
-def computeXY():
+def makeA(anchorsVector):
 	"""
-		-- bruit rand pour simuler les capteurs de distance
-	"""
-	
 
+	"""
+	_x=anchorsVector[0]
+	_y=anchorsVector[1]
+
+	A=[]
+	for i in range(1,len(_x)):
+		A.append([_x[i]-_x[0],_y[i]-_y[0]])
+	return A
+
+#print makeA(vector)
+
+
+def makeB(ray,anchorsVector):
+	b=[]
+	_x=anchorsVector[0]
+	_y=anchorsVector[1]
+
+
+	for i in range(1,len(ray)):
+		b.append(0.5*(ray[0]**2-ray[i]**2+(_x[i]-_x[0])**2+(_y[i]-_y[0])**2))
+	return b
+
+print makeA(vector)
+
+def matMult(A,B):
+	"""
+	"""
+
+def computeXY(sensRay,vector,noiseRate):
+	"""
+	"""
+	x=[]
+	xminusx0=[]
+	A=makeA(vector)
+	B=makeB(sensRay,vector)
+
+
+
+# -------- Functions call
+robotMove()
+xAnchors=vector[0]
+yAnchors=vector[1]
+
+
+plt.plot(xAnchors,yAnchors,"o",markersize=15,color="red")
+
+sensRay=distanceSensors(noise,Ray)
+print Ray
+print sensRay
+
+drawCercles(xAnchors,yAnchors,sensRay)
+#computeXY(sensRay,vector,noise)
+robotMove()
+plt.plot(xAnchors,yAnchors,"o",markersize=15,color="red")
+drawCercles(xAnchors,yAnchors,Ray)
 
 
 
