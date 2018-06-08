3b468b533ba61a8610274a4530d076e2399d03dc
Sami EZZEROUALI <sami.ezzerouali@hotmail.com>
2018-06-08 11:41:46 +0200

    distanceSensors

--- trilaterationSimulation.py
+++ trilaterationSimulation.py
@@ -11,13 +11,13 @@ ________________________________________________________________________________
 |##			Auteur	: EZZEROUALI Sami																	##|
 |##																										##|
 |#########################################################################################################|
-
 """
 
 
 import matplotlib.pyplot as plt
 from numpy import *
-from math import *
+from random import * # randint(0,10) return a random value 0<X<10
+#from math import *
 
 
 
@@ -34,18 +34,17 @@ def anchorsPosition(delta):
 	return _position;
 
 
-
-vector=anchorsPosition(1000)
+delta=1000
+vector=anchorsPosition(delta)
 
 xAnchors=vector[0]
 yAnchors=vector[1]
 
-plt.plot(xAnchors,yAnchors,"o",markersize=15)
+plt.plot(xAnchors,yAnchors,"o",markersize=15,color="red")
 #plt.show()
 
 def drawCercles(_X,_Y,_R):
 	"""
-
 	"""
 	ome=linspace(0,2*pi,720);
 	i=0
@@ -55,29 +54,34 @@ def drawCercles(_X,_Y,_R):
 		for i in range(720):
 			cx.append(_X[k]+_R[k]*sin(ome[i]))
 			cy.append(_Y[k]+_R[k]*cos(ome[i]))
-	plt.plot(cx,cy,"--")
+	plt.plot(cx,cy,"--",color="skyBlue")
 	# plt.show()
 
-Ray=[1000*sqrt(2),1000,0,1000]
+Ray=[1700,1150,200*sqrt(2),1300]
 drawCercles(xAnchors,yAnchors,Ray)
 
 def robotMove():
-	t=linspace(0,10,100)
-	x=100*t
-	y=t**3
-	plt.plot(x,y)
+	t=linspace(0,10,5)
+	x=100*(t)+200
+	y=(t)**3+200
+	plt.plot(x,y,"D",markersize=10,color="green")
 	plt.show()
 robotMove()
+
+def distanceSensors(rate,ray):
+	dvector=[]
+	for i in range(len(ray)):
+		dvector.append(ray[i]+randint(-rate*ray[i],rate*ray[i]))
+	return dvector;
+
+#print distanceSensors(0.1,[1000,1000,1000,1000])
+
 
 def computeXY():
 	"""
 		-- bruit rand pour simuler les capteurs de distance
 	"""
-def distanceSensors():
-	dvector=[]
-
-
-
+	
 
 
 
