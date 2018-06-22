e392b4e21b17edd1aa48b5cfdef46f9bf575446d
Sami EZZEROUALI <sami.ezzerouali@hotmail.com>
2018-06-22 17:35:04 +0200

    Revert "newDeviceAttach makingSmart"

--- circuitBoard/distanceSensing/tag/TAG.ino
+++ circuitBoard/distanceSensing/tag/TAG.ino
@@ -14,7 +14,6 @@ int tagID;
 float tagRange;
 float tagPower;
 
-uint8_t anchorsNum=0;
 
 void setup() {
   
@@ -45,29 +44,10 @@ void newRange() {
 }
 
 void newDevice(DW1000Device* device) {
-  
-  int base[10];
-  int anchorAdd = (int) (DW1000Ranging.getDistantDevice()->getShortAddress());
-  int i=0;
-  while(i<anchorsNum){
-    if(base[i]==anchorAdd){
-      Serial.println("\nknown anchor");
-    }
-    else{
-      base[anchorsNum]=anchorAdd;
-      Serial.print("id,");Serial.print(anchorAdd);
-      anchorsNum++;
-    }
-    i++;
-  }
-
-  
   //Serial.print("\nAnchorID,");
   //Serial.print((int) (DW1000Ranging.getDistantDevice()->getShortAddress()));
 
 }
 
-void inactiveDevice(DW1000Device* device) { 
-  anchorsNum--;
-}
+void inactiveDevice(DW1000Device* device) { }
 
