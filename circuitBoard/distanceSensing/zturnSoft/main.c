caa6a94799b1f66027b0d9440901ae60e403a31c
Sami EZZEROUALI <sami.ezzerouali@hotmail.com>
2018-06-29 13:19:15 +0200

    X

--- circuitBoard/distanceSensing/zturnSoft/main.c
+++ circuitBoard/distanceSensing/zturnSoft/main.c
@@ -3,33 +3,53 @@
 
 #include "matlablite.h"
 
-double in[][3] = {
-	{ 12, -51,   4},
-	{  6, 167, -68},
-	{ -4,  24, -41},
-	{ -1, 1, 0},
-	{ 2, 0, 3},
+
+// x, y, z = 06, 06, 1995
+double Bmat[][1] = {2037,5757,1581,-1047,-3510};
+
+double Amat[][3] = { 
+	{12, -5, 1},
+	{2, -40, 3},
+	{-70, 1, 1},
+	{89, 69, -1},
+	{20, 60, -2},
+	
 };
 
 
 
 int main()
 {
+
+	// sizeof(Amat[0])/sizeof(Amat[0][0]) = 3
+	// sizeof(Bmat)/sizeof(Bmat[0]) = 5 (nombre d'anchors)
+	mat A = matrix_copy(sizeof(Amat[0])/sizeof(Amat[0][0]),Amat,sizeof(Amat)/sizeof(Amat[0]));
+	mat B = matrix_copy(1,Bmat,sizeof(Bmat)/sizeof(Bmat[0]));
+
+	int rank=A->n;
 	mat R, Q;
-	mat x = matrix_copy(3, in, 5);
-	householder(x, &R, &Q);
- 
-	puts("Q"); matrix_show(Q);
+	
+	householder(A, &R, &Q);
+ 	puts("Q"); matrix_show(Q);
 	puts("R"); matrix_show(R);
- 
-	// to show their product is the input matrix
-	mat m = matrix_mul(Q, R);
-	puts("Q * R"); matrix_show(m);
- 
-	matrix_delete(x);
+ 	// to show their product is the input matrix
+	mat Rm1=matrix_new(R->n,R->m);
+	if(matrix_inv(R,Rm1)==0){
+		printf("matrice singuliere, impossible de l'inverser");
+	}
+	else{
+		//mat Rm1=matrix_inv
+		mat Qt=Q;
+		matrix_transpose(Qt);
+		mat x1=matrix_mul(Rm1,Qt);
+		mat x=matrix_mul(x1,B);
+		puts("X :");matrix_show(x);
+	};
+
+
+ 	matrix_delete(A);
 	matrix_delete(R);
 	matrix_delete(Q);
-	matrix_delete(m);
 	
 	return 0;
 }
